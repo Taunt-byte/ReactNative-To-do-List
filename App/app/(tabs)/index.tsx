1cfd883044d98cf59e7a183c277d719f0e326b6f
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2024-07-25 07:02:12 -0300

    Update index.tsx

--- App/app/(tabs)/index.tsx
+++ App/app/(tabs)/index.tsx
@@ -1,148 +1,22 @@
-import React, { useState } from 'react';
-import { Image, StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity, Platform } from 'react-native';
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React, { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 
 export default function HomeScreen() {
-  const [task, setTask] = useState('');
-  const [tasks, setTasks] = useState([]);
-
-  const addTask = () => {
-    if (task.length > 0) {
-      setTasks([...tasks, { key: Math.random().toString(), value: task }]);
-      setTask('');
-    }
-  };
-
-  const removeTask = (taskKey) => {
-    setTasks(tasks.filter(task => task.key !== taskKey));
-  };
-
+  const [text, setText] = useState('');
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <Text style={styles.title}>Lista de Tarefas</Text>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Enter task"
-            style={styles.input}
-            value={task}
-            onChangeText={setTask}
-          />
-          <Button title="ADD" onPress={addTask} />
-        </View>
-        <FlatList
-          data={tasks}
-          renderItem={itemData => (
-            <TouchableOpacity onPress={() => removeTask(itemData.item.key)}>
-              <View style={styles.listItem}>
-                <Text>{itemData.item.value}</Text>
-              </View>
-            </TouchableOpacity>
-          )}
-        />
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={{padding: 10}}>
+      <Text style={{fontSize: 40, textAlign:"center",}}>Lista de Tarefas</Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Qual a boa de hoje?"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 30}}>
+        {text
+          .split(' ')
+          .join(' ')}
+      </Text>
+    </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    width: "80%",
-    padding: 15,
-    backgroundColor: "rgba(242, 242, 242, 0.8)",
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 5,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 15,
-    textAlign: "center",
-    color: "white",
-  },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 15,
-    width: "100%",
-  },
-  input: {
-    width: "75%",
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
-    padding: 8,
-    fontSize: 16,
-  },
-  addButton: {
-    marginLeft: 10,
-  },
-  listItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
-    backgroundColor: "#fff",
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 5,
-    marginVertical: 5,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
-    elevation: 2,
-    width: "100%",
-  },
-  listItemText: {
-    fontSize: 16,
-    color: "#333",
-  },
-});
+};
